da80d792ea9f48c8d6d6d7ac2f98b6ca657106ac
Xaskii <21989858+Xaskii@users.noreply.github.com>
2023-02-11 23:57:50 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react'
 import {
   ChakraProvider,
   Box,
@@ -8,35 +8,68 @@ import {
   Code,
   Grid,
   theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+  Heading,
+  Flex,
+  Button,
+} from '@chakra-ui/react'
+import solaceConnection from './solace-handler'
+import Paho from 'paho-mqtt'
+import options from './solace-credentials'
+// const client = new Paho.Client(
+//   options.invocationContext.host,
+//   Number(options.invocationContext.port),
+//   options.invocationContext.clientId
+// )
+// const client = new Paho.Client(
+//   'mr-connection-rk4tzl653xg.messaging.solace.cloud',
+//   8443,
+//   'example'
+// )
+
+// const onConnect = () => {
+//   console.log('connected')
+//   client.subscribe('button')
+// }
+// // client.onMessageArrived = (message) => {
+// //   console.log(message)
+// // }
+
+// client.connect({
+//   onSuccess: onConnect,
+//   reconnect: true,
+//   userName: 'solace-cloud-client',
+//   password: 'sg835eck7glfctk3plj7k686ei',
+// })
 
 function App() {
+  const [counter, setCounter] = useState(0)
+
+  solaceConnection.connectWithPromise().then((response) => {
+    // console.log('connected:', solaceConnection())
+    solaceConnection.subscribe('exampletopic')
+    solaceConnection.register((message) => {
+      console.log(message)
+    })
+  })
+
+  const handleClick = () => {
+    const message = new Paho.Message(JSON.stringify({ text: 'counterUp' }))
+    message.destinationName = 'exampletopic'
+    solaceConnection.send(message)
+  }
+
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+      <Box textAlign='center' fontSize='xl'>
+        <Flex minH='100vh' alignItems='center' justifyContent='center' p={3}>
+          <VStack>
+            <Heading>{counter}</Heading>
+            <Button onClick={handleClick}>counter</Button>
           </VStack>
-        </Grid>
+        </Flex>
       </Box>
     </ChakraProvider>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/solace-credentials.js
@@ -0,0 +1,16 @@
+const solaceInfo = {
+  userName: 'solace-cloud-client',
+  password: 'i9jm636qoivink3lfm90o7cm1n',
+  invocationContext: {
+    host: 'mr-connection-jj662j6bu41.messaging.solace.cloud',
+    port: 8443,
+    clientId: '',
+  },
+  timeout: 3,
+  keepAliveInterval: 60,
+  cleanSession: true,
+  useSSL: true,
+  reconnect: true,
+}
+
+export default solaceInfo
